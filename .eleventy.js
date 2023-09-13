48d7dc0261407d6e6899baaca88ee00ab12c95f5
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-09-13 10:51:30 -0400

    :sparkles: feat: image shortcode & JS
    - create a nunjucks shortcode to optimize images/icons
    - add nunjucks as markdown template engine
    - add `includes` for layouts directory in exports
    - add passthrough & watch for JS files

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,5 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const eleventyImage = require("@11ty/eleventy-img");
+const eleventyImage = require('@11ty/eleventy-img');
 
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
@@ -7,34 +7,65 @@ module.exports = function(eleventyConfig) {
   // watches scss directory & copies it into dist/css 
   eleventyConfig.addWatchTarget('src/scss/');
   eleventyConfig.addPassthroughCopy('css/');
+  
+  // watches & copies JS files
+  eleventyConfig.addWatchTarget('src/scripts/');
+  eleventyConfig.addPassthroughCopy({ 'src/scripts': 'scripts' });
 
   // copies images
-  eleventyConfig.addPassthroughCopy({ 'src/public/' : 'public/'});
+  eleventyConfig.addPassthroughCopy({ 'src/public/' : 'public/' });
 
-  eleventyConfig.addShortcode("image", async function(src, alt, sizes) {
-		let metadata = await eleventyImage(src, {
-			urlPath: "/public/usedresources",
-      outputDir: "./img/",
-		});
+  // this is the synchronous shortcode, but the individiual words of the alt text
+  // comes in as individual attributes on the image element? but not if you 
+  // console.log the imageAttributes data
+  // eleventyConfig.addNunjucksShortcode("image", function(src, imageClass, alt) {
+  //   if (alt === undefined) {
+  //     throw new Error(`Missing \`alt\` on image from: ${src}`);
+  //   }
 
-		let imageAttributes = {
-			alt,
-			sizes,
-			loading: "lazy",
-			decoding: "async",
-		};
+  //   let imageAttributes = {
+  //     urlPath: 'public/usedresources/' + `${src}`,
+  //     altText: `${alt}`,
+  //     class: `${imageClass}`
+  //   };
+    
+  //   console.log(imageAttributes)
 
-		// You bet we throw an error on a missing alt (alt="" works okay)
-		return eleventyImage.generateHTML(metadata, imageAttributes);
+	// 	return `
+  //     <img src=${imageAttributes.urlPath} alt=${imageAttributes.altText} class=${imageAttributes.class} />
+  //   `;
+	// });
+
+   // this is the asynchronous shortcode (which cannot be used in a macro 😞)
+   eleventyConfig.addNunjucksAsyncShortcode("image", async  function(src, imageClass, altText) {
+    if (altText === undefined) {
+      throw new Error(`Missing \`alt\` on image from: ${src}`);
+    }
+
+    let imageData = await eleventyImage(src, {
+      outputDir: 'dist/public/usedresources',
+      urlPath: '/public/usedresources/',
+      formats: ['svg', 'png', 'jpeg']
+    })
+
+    let imageAttributes = {
+      alt: `${altText}`,
+      class: `${imageClass}`, 
+      loading: 'lazy', 
+      decoding: 'async'
+    };
+
+		return eleventyImage.generateHTML(imageData, imageAttributes);
 	});
 
   // Return your Object options:
   return {
     htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
     dir: {
       input: "src/pages",
       output: "dist",
-      layouts: "../layouts",
+      includes: "../layouts",
     }
   }
 };
