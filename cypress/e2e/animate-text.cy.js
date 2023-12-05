a0c31547cdf9390c683cb34e0db3366760b0adf9
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2023-12-05 08:43:50 -0500

    :rotating_light: fix: linter fixes
    - scss linting errors are fixed
    - add proper class names to tests
    - fix heading assertion

--- cypress/e2e/animate-text.cy.js
+++ cypress/e2e/animate-text.cy.js
@@ -3,16 +3,16 @@ describe('animates the welcome messages', () => {
     cy.visit('/');
   });
   it('visits the homepage', () => {
-    cy.get('h1').should('have.text', "Welcome to my portfolio. I'm an educator turned Front End Developer.");
+    cy.get('.cmp-homepage-hero__heading').contains('Welcome to my portfolio. I\'m an educator turned Front End Developer.');
   });
   it('has an h2 with the class of typewriter and a data-type attribute', () => {
-    cy.get('.typewriter').should('have.attr', 'data-type');
+    cy.get('.js-typewriter').should('have.attr', 'data-type');
   });
   // TODO: this test may need to change once animate-text.js gets refactored
   it('cycles through multiple data-type values', () => {
-    cy.get('.wrap').should('have.text', '');
+    cy.get('.js-wrap').should('have.text', '');
     // eslint-disable-next-line cypress/no-unnecessary-waiting
     cy.wait(2000);
-    cy.get('.wrap').should('not.have.text', '');
+    cy.get('.js-wrap').should('not.have.text', '');
   });
 });
