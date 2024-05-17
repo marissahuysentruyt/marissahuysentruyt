d6239984ec9715d72716fa3d9369a5bbc8d49696
Marissa Huysentruyt <marissa.huysentruyt@heysparkbox.com>
2024-05-17 13:39:50 -0400

    feat: add copyright.js to index.js
    - index.js also no longer includes the resume.js file because designs
    changed. Removing resume.js resolved the console errors

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,2 +1,6 @@
 import './animate-text';
-import './resume';
+import './copyright';
+
+// TODO: turned resume.js off for now, because the tech section of the resume page has been
+// TODO: redesigned. I wanna keep this code though 😊
+// import './resume';
