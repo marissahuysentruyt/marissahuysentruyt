397c2b809a9a565a4f94ed2fee3d4e599e9a21a0
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2024-11-01 19:54:25 -0400

    :wrench: chore: update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,7 +5,7 @@ export default [
   js.configs.recommended,
   pluginCypress.configs.recommended,
   {
-    ignores: ["node_modules", "dist", "/cypress", ".eleventy.js", "*.config.js"]
+    ignores: ["node_modules", "dist", "/cypress", ".eleventy.js", "*.config.js", "img"]
   },
   {
     files: ["src/**/*.js"]
@@ -14,7 +14,7 @@ export default [
     rules: {
       "import/prefer-default-export": 0,
       "no-unused-vars": "warn",
-      "no-plusplus": 0,
+      "no-plusplus": "off",
     }
   },
 ];
