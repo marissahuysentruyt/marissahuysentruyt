54b227ad03d9cb341a7a4f3045aad74935ae2fae
Marissa Huysentruyt <marissahuysentruyt@gmail.com>
2025-04-10 21:12:13 -0400

    :rotating_light: chore: address linter warnings

--- .eleventy.js
+++ .eleventy.js
@@ -1,74 +1,74 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
-const eleventyImage = require('@11ty/eleventy-img');
+const eleventyImage = require("@11ty/eleventy-img");
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
-  // watches scss & scripts directories for changes 
+  // watches scss & scripts directories for changes
   // (scripts in package.json declare where the compiled code goes)
-  eleventyConfig.addWatchTarget('src/scss/');
-  eleventyConfig.addWatchTarget('src/scripts/');
+  eleventyConfig.addWatchTarget("src/scss/");
+  eleventyConfig.addWatchTarget("src/scripts/");
 
   // copies images
-  eleventyConfig.addPassthroughCopy({ 'src/public/' : 'public/' });
+  eleventyConfig.addPassthroughCopy({ "src/public/": "public/" });
 
   // this is the synchronous shortcode and can be used in a macro
-  eleventyConfig.addNunjucksShortcode("image", function(src, imageClass, alt) {
+  eleventyConfig.addNunjucksShortcode("image", function (src, imageClass, alt) {
     if (!src) {
-      throw new Error(`Missing image source`);
+      console.error(`Missing image source`);
+      return "";
     }
 
     if (!alt) {
-      throw new Error(`Missing \`alt\` on image from: ${src}`);
+      console.error(`Missing \`alt\` on image from: ${src}`);
     }
 
-    const imagePath = src.startsWith('/')
-      ? `/public/usedresources${src}`
-      : `/public/usedresources/${src}`;
+    const imagePath = `/public/usedresources/${String(src).replace(/^\//, "")}`;
 
-    console.log(alt);
-
-		return `
-      <img src=${imagePath} alt="${alt}" class=${imageClass || ''} />
+    return `
+      <img src=${imagePath} alt="${alt}" class=${imageClass || ""} />
     `;
-	});
+  });
 
-   // this is the asynchronous shortcode (which cannot be used in a macro 😞)
-   eleventyConfig.addNunjucksAsyncShortcode("asyncImage", async function(src, imageClass, altText) {
-    if (!src) {
-      throw new Error(`Missing image source`);
-    }
-
-    if (altText === undefined) {
-      throw new Error(`Missing \`alt\` on image from: ${src}`);
-    }
-
-    let imageData = await eleventyImage(src, {
-      outputDir: 'dist/public/usedresources',
-      urlPath: '/public/usedresources/',
-      formats: ['svg', 'png', 'webp', 'jpeg'],
-      sharpOptions: {
-        animated: true,
-      },
-      sharpJpegOptions: {
-        quality: 80,
-        progressive: true
-      },
-      cacheOptions: {
-        duration: "1y",
-        directory: ".cache"
+  // this is the asynchronous shortcode (which cannot be used in a macro 😞)
+  eleventyConfig.addNunjucksAsyncShortcode(
+    "asyncImage",
+    async function (src, imageClass, altText) {
+      if (!src) {
+        throw new Error(`Missing image source`);
       }
-    })
 
-    let imageAttributes = {
-      alt: altText,
-      class: imageClass, 
-      loading: 'lazy', 
-      decoding: 'async'
-    };
+      if (altText === undefined) {
+        throw new Error(`Missing \`alt\` on image from: ${src}`);
+      }
 
-		return eleventyImage.generateHTML(imageData, imageAttributes);
-	});
+      let imageData = await eleventyImage(src, {
+        outputDir: "dist/public/usedresources",
+        urlPath: "/public/usedresources/",
+        formats: ["svg", "png", "webp", "jpeg"],
+        sharpOptions: {
+          animated: true,
+        },
+        sharpJpegOptions: {
+          quality: 80,
+          progressive: true,
+        },
+        cacheOptions: {
+          duration: "1y",
+          directory: ".cache",
+        },
+      });
+
+      let imageAttributes = {
+        alt: altText,
+        class: imageClass,
+        loading: "lazy",
+        decoding: "async",
+      };
+
+      return eleventyImage.generateHTML(imageData, imageAttributes);
+    }
+  );
 
   // Return your Object options:
   return {
@@ -78,6 +78,6 @@ module.exports = function(eleventyConfig) {
       input: "src/pages",
       output: "dist",
       includes: "../layouts",
-    }
-  }
+    },
+  };
 };
